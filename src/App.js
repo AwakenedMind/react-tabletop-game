7d0409a5162ffdc80c8e193f25e982f35503f345
Robert Arteaga <robertarteaga@roberts-imac.attlocal.net>
2020-05-03 00:17:23 -0700

    added logic for multiple structures

--- src/App.js
+++ src/App.js
@@ -1,41 +1,147 @@
-import React, { useState, useEffect, useRef, useMemo, useReducer } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import './App.scss';
-import Food from './images/food.png';
-import Summits from './images/summits.png';
-import useHover from './hooks/useHover';
+// import Food from './images/food.png';
+// import Mountain from './images/summits.png';
+// import useHover from './hooks/useHover';
 
-const Tile = React.memo(({ val, handleClick, amount }) => {
-	const [hoverRef, isHovered] = useHover();
+// const images = {
+// 	Mountain: Mountain,
+// 	Wolf: Wolf,
+// 	Empty: null,
+// 	StoneMine: StoneMine,
+// 	MegaStoneMine: MegaStoneMine,
+// 	MegaFarm: MegaFarm,
+// 	Farm: Farm,
+// };
 
-	return val === 1 ? (
-		<li className="tile" onClick={() => handleClick(amount)} ref={hoverRef}>
+const Tile = React.memo(({ type, handleClick, amount }) => {
+	console.log('tile created');
+	return (
+		<li className="tile" onClick={() => handleClick(amount)}>
 			{amount}
-			<img src={Summits} className="summits" />
-			{isHovered ? <div className="modal-hover">Hovering</div> : null}
-		</li>
-	) : (
-		<li
-			className="empty-tile"
-			onClick={() => handleClick(amount)}
-			ref={hoverRef}
-		>
-			{amount}
-			{isHovered ? <div className="modal-hover">Hovering</div> : null}
+			{/* <img src={Summits} className="summits" /> */}
+			{/* {isHovered ? <div className="modal-hover">Hovering</div> : null} */}
 		</li>
 	);
+	// return val === 1 ? (
+	// 	<li className="tile" onClick={() => handleClick(amount)} ref={hoverRef}>
+	// 		{amount}
+	// 		<img src={Summits} className="summits" />
+	// 		{/* {isHovered ? <div className="modal-hover">Hovering</div> : null} */}
+	// 	</li>
+	// ) : (
+	// 	<li
+	// 		className="empty-tile"
+	// 		onClick={() => handleClick(amount)}
+	// 		ref={hoverRef}
+	// 	>
+	// 		{amount}
+	// 		{/* {isHovered ? <div className="modal-hover">Hovering</div> : null} */}
+	// 	</li>
+	// );
 });
 
 const Board = React.memo(({ board, handleClick, memoArr }) => {
 	const [farms, setFarms] = useState(0);
-	const [oilWells, setOilWells] = useState(0);
+	const [megaFarms, setMegaFarms] = useState(0);
+
+	const [stoneMines, setStoneMines] = useState(0);
+	const [megaStoneMines, setMegaStoneMines] = useState(0);
+
 	const [wolves, setWolves] = useState(0);
+	const [alphaWolf, setAlphaWolf] = useState(0);
 
-	const createFarm = () => {};
+	const [emptyTile, setEmptyTile] = useState(0);
+	const [mountain, setMountain] = useState(0);
 
-	const createWolf = () => {};
+	const createRandomNum = () => Math.floor(Math.random() * 100) + 1;
 
-	const createOilWell = () => {};
+	const createFarm = () => {
+		// setFarms(farms + 1);
+		return <Tile type={'Farm'} handleClick={handleClick} amount={150} />;
+	};
+	const createMegaFarm = () => {
+		// setMegaFarms(megaFarms + 1);
 
+		return <Tile type={'MegaFarm'} handleClick={handleClick} amount={650} />;
+	};
+
+	const createWolf = () => {
+		// setWolves(wolves + 1);
+
+		return <Tile type={'Wolf'} handleClick={handleClick} amount={1000} />;
+	};
+	const createAlphaWolf = () => {
+		// setAlphaWolf(alphaWolf + 1);
+		return <Tile type={'AlphaWolf'} handleClick={handleClick} amount={5000} />;
+	};
+
+	const createStoneMine = () => {
+		// setStoneMines(stoneMines + 1);
+		return <Tile type={'StoneMine'} handleClick={handleClick} amount={400} />;
+	};
+
+	const createMegaStoneMine = () => {
+		// setMegaStoneMines(megaStoneMines + 1);
+		return <Tile type={'Farm'} handleClick={handleClick} amount={2000} />;
+	};
+
+	const createEmptyTile = () => {
+		// setEmptyTile(emptyTile + 1);
+		return (
+			<Tile type={'Empty'} handleClick={handleClick} amount={memoArr[count]} />
+		);
+	};
+	const createMountain = () => {
+		// setMountain(mountain + 1);
+		return <Tile type={'Mountain'} handleClick={handleClick} amount={150} />;
+	};
+
+	const createTile = () => {
+		let random = createRandomNum();
+
+		// create an empty tile 50% of the time
+		if (random >= 50) {
+			return createEmptyTile();
+		}
+
+		// 40% chance 0-40
+		else if (random < 50 && random > 40) {
+			// 5% chance to create Mega Farm
+			let megaChance = createRandomNum();
+			if (megaChance <= 5) {
+				return createMegaFarm();
+			} else {
+				return createFarm();
+			}
+		}
+
+		// 19%
+		else if (random > 20 && random <= 40) {
+			// 5% chance to create Mega Oil Well
+			let megaChance = createRandomNum();
+			if (megaChance <= 5) {
+				return createMegaStoneMine();
+			} else {
+				return createStoneMine();
+			}
+		}
+
+		// 10%
+		else if (random <= 20 && random > 9) {
+			// 5% chance to create Alpha Wolf
+			let megaChance = createRandomNum();
+			if (megaChance <= 5) {
+				return createAlphaWolf();
+			} else {
+				return createWolf();
+			}
+		} else {
+			return createMountain();
+		}
+	};
+
+	// keep global count to access array of 100 random values
 	let count = 0;
 
 	return (
@@ -43,14 +149,47 @@ const Board = React.memo(({ board, handleClick, memoArr }) => {
 			{board.map((row, idx1) => {
 				return row.map((tile) => {
 					count = count + 1;
-					if (tile === 1) {
-						// return memoTile;
-						return <Tile val={1} handleClick={handleClick} amount={2500} />;
+
+					let random = createRandomNum();
+
+					// create an empty tile 50% of the time
+					if (random >= 50) {
+						return createEmptyTile();
+					}
+
+					// 40% chance 0-40
+					else if (random < 50 && random > 40) {
+						// 5% chance to create Mega Farm
+						let megaChance = createRandomNum();
+						if (megaChance <= 5) {
+							return createMegaFarm();
+						} else {
+							return createFarm();
+						}
+					}
+
+					// 19%
+					else if (random > 20 && random <= 40) {
+						// 5% chance to create Mega Oil Well
+						let megaChance = createRandomNum();
+						if (megaChance <= 5) {
+							return createMegaStoneMine();
+						} else {
+							return createStoneMine();
+						}
+					}
+
+					// 10%
+					else if (random <= 20 && random > 9) {
+						// 5% chance to create Alpha Wolf
+						let megaChance = createRandomNum();
+						if (megaChance <= 5) {
+							return createAlphaWolf();
+						} else {
+							return createWolf();
+						}
 					} else {
-						// return memoEmptyTile;
-						return (
-							<Tile val={0} handleClick={handleClick} amount={memoArr[count]} />
-						);
+						return createMountain();
 					}
 				});
 			})}
@@ -68,16 +207,16 @@ let generate100 = () => {
 
 export default function App() {
 	const [board, setBoard] = useState([
-		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	]);
 
 	const [gold, setGold] = useState(1000);
@@ -123,15 +262,6 @@ export default function App() {
 	useInterval(() => {
 		setGold((gold) => gold + income);
 	}, 5000);
-
-	const generateRandom100 = () => {
-		return Math.floor(Math.random() * 100) + 1;
-	};
-	// generate an array of 100 random values
-	// let generate100 = () => {
-	// 	return Array.from({ length: 100 }, generateRandom100);
-	// };
-	// let memoizedArr = useMemo(() => generate100());
 
 	return (
 		<div className="container">
