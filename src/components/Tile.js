1b280b156f127a21daf1a6afe92a437454ed287e
Robert Arteaga <robertarteaga@roberts-imac.attlocal.net>
2020-05-03 16:38:35 -0700

    added forwarding ref

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,11 +1,11 @@
 import React, { forwardRef } from 'react';
-import AlphaWolf from '../images/alpha-wolf.png';
-import Wolf from '../images/wolf.png';
-import Farm from '../images/farm.png';
-import MegaFarm from '../images/mega-farm.png';
-import StoneMine from '../images/stone-mine.png';
-import MegaStoneMine from '../images/mega-stone-mine.png';
-import Mountain from '../images/mountain.png';
+import AlphaWolf from '../images/alpha-wolf-colored.png';
+import Wolf from '../images/wolf-colored.png';
+import Farm from '../images/farm-colored.png';
+import MegaFarm from '../images/mega-farm-colored.png';
+import StoneMine from '../images/stone-mine-colored.png';
+import MegaStoneMine from '../images/mega-stone-mine-colored.png';
+import Mountain from '../images/mountain-colored.png';
 
 const images = {
 	Mountain: Mountain,
@@ -18,17 +18,28 @@ const images = {
 	Farm: Farm,
 };
 
-const Tile = ({ type, handleClick, amount, ref }) => {
-	console.log('tile created');
+const Tile = React.forwardRef(({ type, amount }, ref) => {
 	console.log(ref);
+	// console.log('tile created');
+
+	// Updates gold and land when player clicks a tile
+	const handleClick = (amount) => {
+		// console.log('handleClick() ran');
+		console.log(ref.current.data.gold.current);
+		if (ref.current.data.gold.current >= amount) {
+			ref.current.data.gold.current = ref.current.data.gold.current - amount;
+			ref.current.data.land.current = ref.current.data.land.current + 1;
+		}
+	};
+
 	return (
-		<li className="tile" ref={ref} onClick={() => handleClick(amount)}>
+		<li className="tile" onClick={() => handleClick(amount)}>
 			{amount}
 			{type !== 'Empty' ? (
 				<img src={images[type]} className="tile-icon" />
 			) : null}
 		</li>
 	);
-};
+});
 
-export default React.memo(forwardRef(Tile));
+export default Tile;
